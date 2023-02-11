1ad1d076eb8144fb16dad652faa551f9e4189f3c
Samueelx <gsammygitau@gmail.com>
2023-02-11 12:33:15 +0300

    played around with some string and array methods.

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,5 @@
+require 'pry';
+
 #1. #each
 new_array = ["this", "is", "Ruby"].each {|str| puts str.upcase}
 print "#{new_array}\n";
@@ -27,3 +29,19 @@ sorted_users = users.sort do |one, two|
     one[:name] <=> two[:name]
 end 
 pp sorted_users;
+
+# TODO: Write method called #reverse_each_word that takes in a argument of a sentence as a string, 
+# and returns that same sentence with each word reversed in place.
+
+def reverse_each_word (sentence)
+    words = sentence.split(" ")
+    reversed_words = [];
+
+    words.each do |word|
+        reversed_words << word.reverse
+    end
+
+    return reversed_words.join(" ");
+end
+
+puts reverse_each_word("Hello there, and how are you?");
